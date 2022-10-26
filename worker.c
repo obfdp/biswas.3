f8fb62ebac92c1cf6b2af7463c90a93981a7740b
obfdp <obfdp@umsystem.edu>
2022-10-25 22:35:26 -0500

    my commit 3-critical section with message queue

--- worker.c
+++ worker.c
@@ -6,6 +6,12 @@
 #include <sys/shm.h>
 
 
+#define PERMS 0644
+struct my_msgbuf {
+   long mtype;
+   char mtext[200];
+};
+
 /* incrementing share clock by 10000000*/
 int main(int argc, char *argv[])
 {
@@ -19,12 +25,10 @@ int main(int argc, char *argv[])
     char msg[LEN];
     int seed = 0;
 
-    semaphoreGrab();
     srand(time(NULL) + p_id + i); // seed for random sleep time
         
     /* ENTRY Section*/
-      
-    semaphoreWait();
+
     printf("process %d in the Queue\n", p_id);
         
     logfile = fopen(log_filename, "a");
@@ -37,7 +41,7 @@ int main(int argc, char *argv[])
     if (cstest == NULL){
         perror("slave: Error: cstest open failed");
     }
-    /* incrementing share clock by 10000000*/
+/* incrementing share clock by 10000000*/
     for (i =0; i < argc; i++){
   		paddr=paddr*10000000
   		printf("worker_pid %d",getpid());
@@ -45,37 +49,66 @@ int main(int argc, char *argv[])
   		printf(": Incrementing by %p",paddr);
   		printf("|%s| ", argv[i]);
 	}
-        
-    current_time = time (NULL);  	
+        current_time = time (NULL);  	
    	loc_time = localtime (&current_time);
-    fprintf(logfile, "Process %d is in critical section, Timestamp:%s\n", p_id, asctime (loc_time));
+        fprintf(logfile, "Process %d is in critical section, Timestamp:%s\n", p_id, asctime (loc_time));
 
-    seed = (rand() % 5) + 1; 
-    sleep(seed);
+        seed = (rand() % 5) + 1; 
+        sleep(seed);
  	current_time = time (NULL);  	
    	loc_time = localtime (&current_time);
-    strftime (msg, LEN, "%H:%M:%S", loc_time);
-    fprintf(cstest, "%s Queue %d File modified by process number %d\n", msg, i, p_id);
-    fprintf(logfile, "Process id %d writes on shared resource, Timestamp:, cstest %s\n", p_id, asctime (loc_time));
+        strftime (msg, LEN, "%H:%M:%S", loc_time);
+        fprintf(cstest, "%s Queue %d File modified by process number %d\n", msg, i, p_id);
+        fprintf(logfile, "Process id %d writes on shared resource, Timestamp:, cstest %s\n", p_id, asctime (loc_time));
 
-    seed = (rand() % 5) + 1; 
-    sleep(seed);
+        seed = (rand() % 5) + 1; 
+        sleep(seed);
 	current_time = time (NULL);   	
    	loc_time = localtime (&current_time);
-    fprintf(logfile, "Process id: %d exits critical section, Timestamp: %s\n", p_id, asctime (loc_time));
-    fclose(logfile);
-    fclose(cstest);
-    logfile = NULL;
-    cstest = NULL;
+        fprintf(logfile, "Process id: %d exits critical section, Timestamp: %s\n", p_id, asctime (loc_time));
+        fclose(logfile);
+        fclose(cstest);
+        logfile = NULL;
+        cstest = NULL;
        
     /* EXIT Section */
-    printf("process %d finished the job\n", p_id);
-    semaphoreSignal();
-
-
+       printf("process %d finished the job\n", p_id);
+    
 	printf("\nChild is now ending.\n");
-	sleep(3);
+	sleep(1);
 	return EXIT_SUCCESS;
-
 }
+
+void msgrcv()
+{
+	struct my_msgbuf buf;
+   int msqid;
+   int toend;
+   key_t key;
+   
+   if ((key = ftok("msgq.txt", 'B')) == -1) {
+      perror("ftok");
+      exit(1);
+   }
+   
+   if ((msqid = msgget(key, PERMS)) == -1) { /* connect to the queue */
+      perror("msgget");
+      exit(1);
+   }
+   printf("message queue: ready to receive messages.\n");
+   
+   for(;;) { 
+      if (msgrcv(msqid, &buf, sizeof(buf.mtext), 0, 0) == -1) {
+         perror("msgrcv");
+         exit(1);
+      }
+      printf("recvd: \"%s\"\n", buf.mtext);
+      toend = strcmp(buf.mtext,"end");
+      if (toend == 0)
+      break;
+   }
+   printf("message queue: done receiving messages.\n");
+   system("rm msgq.txt");
+}
+
 
